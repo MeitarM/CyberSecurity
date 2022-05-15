39bfd769225b4f66c2f48f20c315ebc975e787da
MeitarM <105547530+MeitarM@users.noreply.github.com>
2022-05-15 12:26:17 +0300

    Add files via upload

---
+++ Database.php
@@ -0,0 +1,385 @@
+<?php
+session_start(); 
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+  <center>
+    <title>INI Database</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    html,body,h1,h2,h3,h4 {font-family:"Lato", sans-serif}
+    .mySlides {display:none}
+    .w3-tag, .fa {cursor:pointer}
+    .w3-tag {height:15px;width:15px;padding:0;margin-top:6px}
+    </style>
+  </head><center>
+<body>
+
+<!-- Links (sit on top) -->
+<div class="w3-top">
+  <div class="w3-row w3-large w3-light-grey">
+    <div class="w3-col s3">
+      <a href="#" class="w3-button w3-block">Projects</a>
+    </div>
+    <div class="w3-col s3">
+      <a href="#plans" class="w3-button w3-block">Finances</a>
+    </div>
+    <div class="w3-col s3">
+      <a href="#Workers" class="w3-button w3-block">Workers</a>
+    </div>
+    <div class="w3-col s3">
+      <a href="#Upload" class="w3-button w3-block">Files Upload</a>
+    </div>
+  </div>
+</div>
+
+<!-- Content -->
+<div class="w3-content" style="max-width:1100px;margin-top:80px;margin-bottom:80px">
+
+  <div class="head">
+    <h1 style="background-color:Black;">INI Database</h1>
+  </div>
+<div class="text">	
+<h3><b>Administrators Information</b></h3>
+Welcome Administrators of INI! this is a sensitive page which belongs to you and only to your eyes and should not be shared with unauthorized personal, thank you and proceed with caution.any problems that arise,please contact the IT department.
+</p></div>
+
+
+  
+  <!-- Grid -->
+
+      <span class="head"><p>Projects</p></span>
+    </div>
+    <div class="w3-col l3 m6 w3-light-grey w3-container w3-padding-16">
+      <h3>Al Maktoum International Airport, Dubai </h3>
+      <p>No other airport would prepare you for the scale of Dubai's Al Maktoum International Airport, which extends over more than 21 square miles. The facility is designed to handle 200 wide-body aircraft at a time. The airport's second expansion phase alone has an estimated cost of more than $32 billion. Originally scheduled for completion in 2018, the latest expansion phase has been delayed, with no definite completion date.INI oversees the operation 24/7 with the best teams around the world. </p>
+    </div>
+
+    <div class="w3-col l3 m6 w3-grey w3-container w3-padding-16">
+      <h3>Great Man-Made River Project, Libya</h3>
+      <p>The “Great Man-Made River” (GMR) has been an ongoing project since 1985. To this day, it is classed as the largest irrigation project in the world, and upon completion, it will supply water to more than 350,000 acres of arable land. Not only will it assist Libyan farmers with their produce, but it will also increase the availability of clean drinking water in the majority of Libya’s urban centres. 
+
+The water source of the GMR is the Nubian Sandstone Aquifer System ─ the world’s largest-known fossil water aquifer system ─ and the project’s completion is slated for 2030.</p>
+    </div>
+
+    <div class="w3-col l3 m6 w3-dark-grey w3-container w3-padding-16">
+      <h3>Chuo Shinkansen, Japan</h3>
+      <p>Moving swiftly along the worldwide rail network, Japan’s ─ officially named ─ Linear Chuo Shinkansen, high-speed rail line will transport passengers 286 kilometres from Tokyo to Nagoya in a mere 40 minutes. The trains on this line will power along at approximately 313 miles per hour and are based on the country’s world-leading Superconducting Magnetic Levitation (Maglev) system. 
+Approximately 86 per cent of the new line will be underground, so it will require extensive tunnel construction. The project is scheduled for completion by 2027. </p>
+    </div>
+
+    <div class="w3-col l3 m6 w3-black w3-container w3-padding-16">
+      <h3>Jubail II, Saudi Arabia</h3>
+      <p>Jubail II is a 22-year-long project which, three decades ago, promised to raise an industrial city from the desert sands the largest civil engineering project in the world ─ kicked off in 2014, with an expansion budget of US$11bn Once the project is completed, Jubail will comprise at least one hundred industrial plants, an 800,000-cubic-metre desalination plant, more than 530 miles of railway and road, alongside a 350,000-barrel-a-day oil refinery. The project should be wrapped up in 2024, with over 50,000 residential units being added into the industry city by 2026 INI bought the project.</p>
+    </div>
+  </div>
+
+  <!-- Grid -->
+  <div class="w3-row-padding" id="plans">
+    <div class="w3-center w3-padding-64">
+      <h3><div class="head">Finances</h3></div>
+      <img src="Cash.jpg"style="width:500px;height:500px;">
+      <img src="Balance.jpg"style="width:500px;height:500px;">
+      <img src="Income.jpg"style="width:500px;height:500px;">
+      <br>
+      <div class="text">
+Vanguard Group, Inc. (The): 62,448,572	Dec 30, 2021	6.03%	65,994,401,918<br> &nbsp;
+Blackrock Inc.:				52,844,995	Dec 30, 2021	5.10%	55,845,533,816<br> &nbsp;
+Capital World Investors:		36,665,256	Dec 30, 2021	3.54%	38,747,109,235<br> &nbsp;
+State Street Corporation:	32,465,720	Dec 30, 2021	3.13%	34,309,123,581<br> &nbsp;
+Geode Capital Management, LLC:	13,571,554	Dec 30, 2021	1.31%	14,342,146,836<br> &nbsp;
+FMR, LLC:					10,988,029	Dec 30, 2021	1.06%	11,611,929,286<br> &nbsp;
+Baillie Gifford and Company	:10,798,014	Dec 30, 2021	1.04%	11,411,125,234<br> &nbsp;
+Jennison Associates LLC	:	10,627,050	Dec 30, 2021	1.03%	11,230,453,899<br> &nbsp;
+Northern Trust Corporation:	8,558,197	Dec 30, 2021	0.83%	9,044,131,425<br> &nbsp;
+Price (T.Rowe) Associates Inc:	8,149,225	Dec 30, 2021	0.79%	8,611,937,995<br> &nbsp;
+	</div>
+    </div>
+
+
+
+  
+
+  <!-- Grid -->
+  <div class="w3-row-padding" id="Workers">
+    <div class="head">
+      <h3>Workers</h3>
+    </div>
+    </div>
+       <div class="text"><h3>CEO Sarah Hoffman</h3>
+Gender: female<br>
+Race: White<br>
+Birthday: 7/10/1969 (40 years old)<br>
+Street: 3971 Clay Lick Road<br>
+City, State, Zip: Aurora, Colorado(CO), 80014<br>
+Telephone: 720-624-4316<br>
+Mobile: 720-810-8600<br>
+
+<div class="float-parent-element">
+  <div class="float-child-element">
+    <div class="black"><h3>Ayelet Dolev</h3>
+Gender: female<br>
+Race: Asian<br>
+Birthday: 11/5/1998 (28 years old)<br>
+Street: 4768 Mcwhorter Road<br>
+City, State, Zip: Houston, Mississippi, 38851<br>
+Telephone: 662-456-5567<br>
+Mobile: 662-542-0633<br>
+
+    <div class="black"><h3>Moshe Zangliblaf</h3>
+Gender:male<br>
+Race: Asian<br>
+Birthday: 11/5/1998 (57 years old)<br>
+Street: 4768 Mcwhorter Road<br>
+City, State, Zip: Israel, Jerusalem, 74851<br>
+Telephone: 662-456-5567<br>
+Mobile: 662-542-0633<br>
+.</p></div>
+  </div>
+  <div class="float-child-element">
+    <div class="blue"><h3>Ido Menahem</h3>
+<p>Gender: male<br>
+Race: Hispanic<br>
+Birthday: 9/19/1989 (32 years old)<br>
+Street: 2192 Franklin Street<br>
+City, State, Zip: Dothan, Alabama(AL), 36303<br>
+Telephone: 334-743-2056<br>
+Mobile: 334-200-1657<br>.</p></div>
+
+
+
+<div class="float-parent-element">
+  <div class="float-child-element">
+    <div class="red"><h3>Alexander Politov</h3>
+<p>Gender: male<br>
+Race: Asian<br>
+Birthday: 12/25/1992 (29 years old)<br>
+Street: 1673 Sunburst Drive<br>
+City, State, Zip: Fort Myers, Florida(FL), 33901<br>
+Telephone: 239-274-3577<br>
+Mobile: 305-923-9425<br>.</p></div>
+  </div>
+  <div class="float-child-element">
+    <div class="yellow"><h3>Ron Knaifel</h3>
+<p>Gender: male<br>
+Race: Hispanic<br>
+Birthday: 9/19/1989 (32 years old)<br>
+Street: 2192 Franklin Street<br>
+City, State, Zip: Dothan, Alabama(AL), 36303<br>
+Telephone: 334-743-2056<br>
+Mobile: 334-200-1657<br>.</p></div>
+  </div>
+</div>
+
+</style>
+<h3>David Moshayev</h3>
+<p>
+Gender: male<br>
+Race: Black<br>
+Birthday: 7/9/1954 (67 years old)<br>
+Street: 1788 Sun Valley Road<br>
+City, State, Zip: Spokane, Washington(WA), 99201<br>
+Telephone: 509-835-3853<br>
+Mobile: 509-991-7983<br>.</p>
+
+<h3>Tomer Jacob Israeli</h3>
+<p>Gender: male<br>
+Race: Hispanic<br>
+Birthday: 6/23/1984 (37 years old)<br>
+Street: 2905 Park Boulevard<br>
+City, State, Zip: Ferguson, Iowa(IA), 50078<br>
+Telephone: 641-478-1015<br>
+Mobile: 641-416-2645<br>.</p>
+</div>
+
+<h3>Ran Assis</h3>
+<p>Gender: male<br>
+Race: White<br>
+Birthday: 1/21/1999 (23 years old)<br>
+Street: 1172 Joanne Lane<br>
+City, State, Zip: Sudbury, Massachusetts(MA), 01776<br>
+Telephone: 978-507-6824<br>
+Mobile: 978-217-2363<br>.</p>
+
+<h3>Ben Alankva</h3>
+<p>Gender: male<br>
+Race: White<br>
+Birthday: 1/21/1999 (23 years old)<br>
+Street: 1172 Joanne Lane<br>
+City, State, Zip: Sudbury, Massachusetts(MA), 01776<br>
+Telephone: 978-507-6824<br>
+Mobile: 978-217-2363<br>.</p>
+
+
+<h3>Emilio</h3>
+<p>Gender: male<br>
+Race: White<br>
+Birthday: 1/21/1999 (23 years old)<br>
+Street: 1172 Joanne Lane<br>
+City, State, Zip: Sudbury, Massachusetts(MA), 01776<br>
+Telephone: 978-507-6824<br>
+Mobile: 978-217-2363<br>.</p>
+
+<h3>Zvika Itzhak</h3>
+<p>Gender: male<br>
+Race: White<br>
+Birthday: 1/21/1999 (23 years old)<br>
+Street: 1172 Joanne Lane<br>
+City, State, Zip: Sudbury, Massachusetts(MA), 01776<br>
+Telephone: 978-507-6824<br>
+Mobile: 978-217-2363<br>.</p>
+
+<h3>Nissim Batesh</h3>
+<p>Gender: male<br>
+Race: White<br>
+Birthday: 1/21/1999 (23 years old)<br>
+Street: 1172 Joanne Lane<br>
+City, State, Zip: Sudbury, Massachusetts(MA), 01776<br>
+Telephone: 978-507-6824<br>
+Mobile: 978-217-2363<br>.</p>
+
+<h3>Valentin</h3>
+<p>Gender: male<br>
+Race: White<br>
+Birthday: 1/21/1999 (23 years old)<br>
+Street: 1172 Joanne Lane<br>
+City, State, Zip: Sudbury, Massachusetts(MA), 01776<br>
+Telephone: 978-507-6824<br>
+Mobile: 978-217-2363<br>.</p>
+
+<h3>Igor</h3>
+<p>Gender: male<br>
+Race: White<br>
+Birthday: 1/21/1999 (23 years old)<br>
+Street: 1172 Joanne Lane<br>
+City, State, Zip: Sudbury, Massachusetts(MA), 01776<br>
+Telephone: 978-507-6824<br>
+Mobile: 978-217-2363<br>.</p>
+
+<h3>Adi INT college</h3>
+<p>Gender: female<br>
+Race: White<br>
+Birthday: 1/21/1999 (23 years old)<br>
+Street: 1172 Joanne Lane<br>
+City, State, Zip: Sudbury, Massachusetts(MA), 01776<br>
+Telephone: 978-507-6824<br>
+Mobile: 978-217-2363<br>.</p>
+</div>
+</div>
+</div>
+</p>
+
+​<style>
+.head {
+  background-color: darkred;
+  color: gold;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 30px;
+  margin: 4px 2px;
+  border-radius: 8px;
+}
+
+.text {
+  background-color: black;
+  border: none;
+  color: gold;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 8px;
+}
+
+
+.button {
+  background-color: Indigo;
+  border: none;
+  color: Lime;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 50%;
+  transition-duration: 0.9s;
+}
+.button:hover {
+  background-color: Lime; 
+  color: Indigo;
+}
+
+</style>
+
+  <!-- Upload -->
+  <div class="w3-center w3-padding-64" id="Upload">
+    <span class="head">File Upload</span>
+  </div>
+  
+ 
+
+      <div class="text">Fellow admins! upload the INI projects and delicate files here.
+      <p>Click on the "Choose File" button to choose a file to upload:</div></p>
+	<?php
+    	if (isset($_SESSION['message']) && $_SESSION['message'])
+    	{
+      		echo '<p class="notification">'.$_SESSION['message'].'</p>';
+      		unset($_SESSION['message']);
+    	}
+    	
+    ?>
+    
+    <form method="POST" action="upload.php" enctype="multipart/form-data">
+    	
+    	<div>
+      	<span class="file-name"></span>
+      	<label for="file-upload"><input type="file" class="button" id="file-upload" name="uploadedFile"></label>
+    	</div>	
+    	<input type="submit" name="uploadBtn" class="button" value="Upload" />
+    	
+  	</form>
+  	<div class="text">
+  	<?php
+		echo "Uploaded Files:";
+		$path = "/var/www/html/INI/Uploads/";
+		$dh = opendir($path);
+		$i=1;
+		while (($file = readdir($dh)) !== false) {
+    		if($file != "." && $file != "..") {
+       			echo "<br/>$file";
+      			$i++;
+  		  }
+		}
+		
+	closedir($dh);
+	?> 
+	</div>
+    <form action="Launch.php">
+    	<input type="submit" class="button" value="Execute File">
+    </form>
+    <style>
+html { 
+  background: url(data.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
+
+
+
+</body>
+</html>
